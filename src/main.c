b55133945df49fe18ffeee6ce91f543e1f76e0fb
kwzanella <joaogabrielkondazanella@gmail.com>
2024-01-04 22:41:51 -0300

    added wifi code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/ledc.h"
+
+#include "secrets.h"
+
 /*
  * -------------------------------------------
  *              RFID-RC522      ESP-WROOM-32
@@ -29,19 +43,82 @@
 
 #define BUZZER_CHANNEL     LEDC_CHANNEL_0
 #define DUTY_RESOLUTION    LEDC_TIMER_13_BIT
+#define BUZZER_DUTY        (uint16_t)  8191  // Determines the volume. Max value is 2**LEDC_TIMER_13_BIT
 
 #define LED_DELAY          (uint16_t)  200
-
 #define TONE_DURATION      (uint16_t)  200
 #define ACTION_DELAY       (uint16_t)  500
 
 #define SUCCESS_FREQUENCY  (uint32_t)  1000
 #define FAILURE_FREQUENCY  (uint32_t)  500
-#define BUZZER_DUTY        (uint16_t)  8191  // Determines the volume. Max value is 2**LEDC_TIMER_13_BIT
 
+#define WIFI_MAX_RETRY 5
 
-
+static const char *TAG = "wifi station";
+static int s_retry_num = 0;
 uint8_t s_led_state = 0;
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data) {
+
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {  // start connection
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {  // wifi failed
+        if (s_retry_num < WIFI_MAX_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else {
+            ESP_LOGI(TAG, "Failed to connect to AP");
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {  // wifi connected
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+
+        ESP_LOGI(TAG, "connected to AP");
+    }
+}
+
+void wifi_init_sta(void) {
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASSWORD,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+}
 
 void configure_led(void) {
     gpio_reset_pin(RED_LED);
@@ -49,7 +126,9 @@ void configure_led(void) {
 }
 
 void blink_led(void) {
-    gpio_set_level(RED_LED, s_led_state);
+    gpio_set_level(RED_LED, 1);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    gpio_set_level(RED_LED, 0);
 }
 
 void init_buzzer() {
@@ -69,6 +148,12 @@ void init_buzzer() {
         .intr_type = LEDC_INTR_DISABLE,
         .timer_sel = LEDC_TIMER_0,
         .duty = 0,
+        /* 
+        * IMPORTANT!!!!
+        * hpoint has to be set to something or the buzzer will have undefined behavior
+        * Refer to the LEDC API reference and "LED PWM Controller (LEDC)" chapter in the ESP32 technical reference
+        */
+        .hpoint = 8190,
     };
     ledc_channel_config(&ledc_channel);
 }
@@ -85,12 +170,28 @@ void play_tone(const uint32_t frequency, const uint16_t duration) {
     ledc_update_duty(LEDC_HIGH_SPEED_MODE, BUZZER_CHANNEL);
 }
 
-void app_main() {
-
+void blink_led_task(void *pvParameters) {
     while (1) {
         blink_led();
-        s_led_state = !s_led_state;
         play_tone(SUCCESS_FREQUENCY, 1000);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
+
+// Meant to initialize and start main functionality
+void app_main(void) {
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    configure_led();
+    init_buzzer();
+    xTaskCreate(&blink_led_task, "blink_led_task", 2048, NULL, 5, NULL);
 }
